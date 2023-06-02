d1f1e2ff8d38432c845aa8babfc5a52dcecb5699
fedenilles <fedenilles@gmail.com>
2023-06-02 17:34:06 -0300

    subiendo radio button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import TextInput from './components/TextInput'
+import Checkbox from './components/Checkbox'
+import Select from './components/Select'
+import Radio from './components/Radio'
+
+const validate = (values) => {
+    const errors = {}
+
+    if(!values.name){
+      errors.name = 'requerido'
+    } else if (values.name.length < 5) {
+      errors.name = 'El Nombre es muy corto'
+    }
+    if(!values.lastname){
+      errors.lastname = 'requerido'
+    } else if (values.lastname.length < 8) {
+      errors.lastname = 'El Nombre es muy corto'
+    }
+
+    if(!values.radio) {
+      errors.radio = 'Reqerido'
+    }
+      
+    return errors
+  }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+    initialValues={{ name: '', lastname: '', email: '', radio: '' }}
+    validate={validate}
+    onSubmit={values => console.log(values)}
+    >
+      <Form>
+        <TextInput name='name' label='Nombre'/>
+        <br/>
+        <TextInput name='lastname' label='Apellido'/>
+        <br/>
+        <TextInput name='email' label='Email'/>
+        <br/>
+        <Select label= 'Sexo' name='sexo'>
+          <option value= ''>Seleccione Sexo</option>
+          <option value= 'hombre'>Hombre</option>
+          <option value= 'mujer'>Mujer</option>
+          <option value= 'indefinido'>Indefinido</option>
+        </Select>
+        <Checkbox name='accept'>
+          Aceptar terminos y Condiciones
+        </Checkbox>
+        <Radio name= 'radio' value='pelota' label='Pelota' />
+        <Radio name= 'radio' value='palo' label='Palo' />
+        <Radio name= 'radio' value='escalera' label='escalera' />
+        <ErrorMessage name='radio'/>
+        <button type='submit'>Enviar</button>
+      </Form>
+     
+    </Formik>
   );
 }
 
